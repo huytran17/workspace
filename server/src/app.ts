546ab718937b893c82cfb6e7ba29e9cdaf20da7e
huytran17 <alpha.lloyd1368@gmail.com>
2023-09-16 17:50:03 +0700

    init server

---
+++ server/src/app.ts
@@ -0,0 +1,18 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(process.env.BASE_PORT, () =>
+  console.log(`Server listening on port ${process.env.BASE_PORT}`)
+);
