4ff0cd274385aad1318244158a655ce97a3bd192
huytran17 <alpha.lloyd1368@gmail.com>
2023-09-17 14:41:52 +0700

    link schema

--- server/src/database/schemas/link.ts
+++ server/src/database/schemas/link.ts
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 import mongooseLeanVirtuals from "mongoose-lean-virtuals";
+import ILink from "../interfaces/link";
 
 const Schema = mongoose.Schema;
 
-const linkSchema = new Schema({
+const linkSchema = new Schema<ILink>({
   title: { type: String, required: true, trim: true },
   uri: { type: String, required: true, trim: true },
   created_at: { type: Date, default: new Date() },
