90b744171d3fc5281ad55a3d7d39e5fbea8f5dd4
huytran17 <alpha.lloyd1368@gmail.com>
2023-09-17 14:41:58 +0700

    notification schema

--- server/src/database/schemas/notification.ts
+++ server/src/database/schemas/notification.ts
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 import mongooseLeanVirtuals from "mongoose-lean-virtuals";
-import { NotificationType, Status } from "../interfaces/notification";
+import INotification, {
+  NotificationType,
+  Status,
+} from "../interfaces/notification";
 
 const Schema = mongoose.Schema;
 
-const notificationSchema = new Schema({
+const notificationSchema = new Schema<INotification>({
   type: { type: String, required: true, trim: true, enum: NotificationType },
   status: { type: String, required: true, trim: true, enum: Status },
-  task: { type: Object, required: true },
+  task: { type: Schema.Types.ObjectId, required: true },
   content: { type: String, required: true, trim: true },
   created_at: { type: Date, default: new Date() },
   updated_at: { type: Date, default: null },
