0073c899737499499e8ed17a1959f5d49c3cdf83
huytran17 <alpha.lloyd1368@gmail.com>
2023-09-17 14:42:03 +0700

    status schema

---
+++ server/src/database/schemas/status.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import mongooseLeanVirtuals from "mongoose-lean-virtuals";
+import IStatus from "../interfaces/status";
+
+const Schema = mongoose.Schema;
+
+const statusSchema = new Schema<IStatus>({
+  title: { type: String, required: true, trim: true },
+  color: { type: String, required: true, trim: true },
+  created_at: { type: Date, default: new Date() },
+  updated_at: { type: Date, default: null },
+  deleted_at: { type: Date, default: null },
+});
+
+statusSchema.index({ created_at: -1 });
+
+statusSchema.plugin(mongooseLeanVirtuals);
+
+export default statusSchema;
