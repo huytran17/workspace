fa999dc4956ab6c3c3d38d3049c6d1507e2127e4
huytran17 <alpha.lloyd1368@gmail.com>
2023-09-17 14:42:18 +0700

    user schema

--- server/src/database/schemas/user.ts
+++ server/src/database/schemas/user.ts
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 import mongooseLeanVirtuals from "mongoose-lean-virtuals";
+import IUser from "../interfaces/user";
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  fullName: { type: String, required: true, trim: true },
+const userSchema = new Schema<IUser>({
+  fullname: { type: String, required: true, trim: true },
   email: { type: String, required: true, trim: true },
   hash_password: { type: String, required: true, trim: true },
   ip: { type: Object },
