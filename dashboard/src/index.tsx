7dc4e3e34901ed63cabd8c44d898b7be87abd052
huytran17 <alpha.lloyd1368@gmail.com>
2023-11-12 18:48:18 +0700

    app style

--- dashboard/src/index.tsx
+++ dashboard/src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
+import { Provider } from "react-redux";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
 import store from "./store";
+import "./styles/index.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
