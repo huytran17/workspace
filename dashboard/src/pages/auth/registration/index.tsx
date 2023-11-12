f6ae1af2ff65675c64dafa76c9c6dd1bb3c2cc33
huytran17 <alpha.lloyd1368@gmail.com>
2023-11-12 17:59:51 +0700

    test redux

---
+++ dashboard/src/pages/auth/registration/index.tsx
@@ -0,0 +1,8 @@
+import BaseRegistrationForm from "@/components/auth/BaseRegistrationForm";
+import { FC } from "react";
+
+const RegistrationPage: FC<{}> = () => {
+  return <BaseRegistrationForm />;
+};
+
+export default RegistrationPage;
