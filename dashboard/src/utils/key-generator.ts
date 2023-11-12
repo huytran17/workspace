aa33ea048f4d16581b630185e1561d6c78deb452
huytran17 <alpha.lloyd1368@gmail.com>
2023-11-12 16:42:38 +0700

    key generator

---
+++ dashboard/src/utils/key-generator.ts
@@ -0,0 +1,24 @@
+import { Key } from "react";
+
+const generateKey = (): Key | null | undefined => {
+  const S4 = function (): string {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  };
+
+  return (
+    S4() +
+    S4() +
+    "-" +
+    S4() +
+    "-" +
+    S4() +
+    "-" +
+    S4() +
+    "-" +
+    S4() +
+    S4() +
+    S4()
+  );
+};
+
+export { generateKey };
