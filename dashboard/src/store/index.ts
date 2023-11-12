8648b5c4c8e56f9f5d89bb912716932bd3e771ef
huytran17 <alpha.lloyd1368@gmail.com>
2023-11-12 17:58:55 +0700

    provide app store

--- dashboard/src/store/index.ts
+++ dashboard/src/store/index.ts
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./auth/slice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type RootDispatch = typeof store.dispatch;
+
+export default store;
