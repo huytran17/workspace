c28c7389ad43319ed369079d98896fb6a9449548
huytran17 <alpha.lloyd1368@gmail.com>
2023-11-25 14:08:39 +0700

    suspense loading app

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,20 +1,23 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { routes } from "./routes";
 import { keyGenerator } from "./utils/key-generator";
+import { Suspense } from "react";
 
 export default function App() {
   return (
     <Router>
       <div className="App">
-        <Routes>
-          {routes.map((route) => (
-            <Route
-              key={keyGenerator()}
-              path={route.path}
-              Component={route.component}
-            ></Route>
-          ))}
-        </Routes>
+        <Suspense>
+          <Routes>
+            {routes.map((route) => (
+              <Route
+                key={keyGenerator()}
+                path={route.path}
+                Component={route.component}
+              ></Route>
+            ))}
+          </Routes>
+        </Suspense>
       </div>
     </Router>
   );
