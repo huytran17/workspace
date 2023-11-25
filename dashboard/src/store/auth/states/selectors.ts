5c4a18309eaa29dcabd6a3816c05f45fffe72726
huytran17 <alpha.lloyd1368@gmail.com>
2023-11-25 14:38:14 +0700

    add more actions to redux auth

--- dashboard/src/store/auth/states/selectors.ts
+++ dashboard/src/store/auth/states/selectors.ts
@@ -2,6 +2,7 @@ import { RootState } from "@/store";
 
 const authSelectors = {
   selectHasUser: (state: RootState) => state.auth.hasUser,
+  selectUser: (state: RootState) => state.auth.user,
 };
 
 export default authSelectors;
