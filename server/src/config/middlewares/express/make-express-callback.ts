7de8273ebcf4ccfd939349dd713f85a8f23c0437
huytran17 <alpha.lloyd1368@gmail.com>
2023-11-26 15:11:49 +0700

    update express callback

--- server/src/config/middlewares/express/make-express-callback.ts
+++ server/src/config/middlewares/express/make-express-callback.ts
@@ -1,5 +1,6 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction, response } from "express";
 import { http_status } from "@/config/constants/http-status";
+import { get } from "lodash";
 
 export default function makeExpressCallback(controller: Function) {
   return async function (req: Request, res: Response, next: NextFunction) {
@@ -8,13 +9,28 @@ export default function makeExpressCallback(controller: Function) {
         validated: { ...req.body, ...req.params, ...req.query },
         user: req.user,
         files: req.files,
+        query: req.query,
+        params: req.params,
+        body: req.body,
+        method: req.method,
+        headers: {
+          "Content-Type": req.get("Content-Type"),
+          Referer: req.get("referer"),
+          "User-Agent": req.get("User-Agent"),
+        },
       };
 
-      const result = await controller(httpRequest);
-
-      res.status(http_status.OK).json(result);
-
-      next();
+      controller(httpRequest)
+        .then((response: any) => {
+          response.headers && res.set(response.headers);
+          res.type("json");
+          res.status(response.statusCode).send(response.body);
+          next();
+        })
+        .catch((error: any) => {
+          res.status(error.statusCode).send(error.body);
+          next(error);
+        });
     } catch (error) {
       console.error(error);
       res.status(http_status.INTERNAL_SERVER_ERROR).json(error);
