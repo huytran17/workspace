c731caba34ca3bd74c4a499c127f5e6053029c72
huytran17 <alpha.lloyd1368@gmail.com>
2023-12-05 21:06:28 +0700

    cleanup code

--- server/src/use-cases/user/create-user.ts
+++ server/src/use-cases/user/create-user.ts
@@ -1,10 +1,10 @@
 import IUserDb from "@/data-access/interfaces/user-db";
 import IUser from "@/database/interfaces/user";
 
-type OmittedFields = "_id" | "created_at" | "updated_at" | "deleted_at";
+type OmitProps = "_id" | "created_at" | "updated_at" | "deleted_at";
 
 interface IUserDetails {
-  userDetails: Omit<IUser, OmittedFields>;
+  userDetails: Omit<IUser, OmitProps>;
 }
 
 export type CreateUser = ({ userDetails }: IUserDetails) => Promise<IUser>;
