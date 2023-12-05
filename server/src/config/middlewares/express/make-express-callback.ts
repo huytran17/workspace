fb092a5fcd903d0cf36d7eaa4593e49d561867a5
huytran17 <alpha.lloyd1368@gmail.com>
2023-12-05 21:09:49 +0700

    cleanup code

--- server/src/config/middlewares/express/make-express-callback.ts
+++ server/src/config/middlewares/express/make-express-callback.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction, response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { http_status } from "@/config/constants/http-status";
 
 export default function makeExpressCallback(controller: Function) {
