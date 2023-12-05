a606d93e9bc1066605789ce3d6f431e67b3ad873
huytran17 <alpha.lloyd1368@gmail.com>
2023-12-05 21:56:02 +0700

    login route

--- dashboard/src/routes.ts
+++ dashboard/src/routes.ts
@@ -2,6 +2,9 @@ import { ComponentType, lazy } from "react";
 const BaseRegistrationForm = lazy(
   () => import("./components/auth/registration/BaseRegistrationForm")
 );
+const BaseLoginForm = lazy(
+  () => import("./components/auth/login/BaseLoginForm")
+);
 
 interface IRoute {
   path: string;
@@ -12,6 +15,10 @@ const routes: IRoute[] = [
   {
     path: "/auth/registration",
     component: BaseRegistrationForm,
+  },
+  {
+    path: "auth/login",
+    component: BaseLoginForm,
   },
 ];
 
