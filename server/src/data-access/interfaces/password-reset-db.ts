71a35699c4ddb87657747e3dbbf7327a0b0e1064
huytran17 <alpha.lloyd1368@gmail.com>
2023-12-13 22:02:19 +0700

    password-reset db interface

---
+++ server/src/data-access/interfaces/password-reset-db.ts
@@ -0,0 +1,16 @@
+import IPasswordReset from "@/database/interfaces/password-reset";
+
+export default interface IPasswordResetDb {
+  findByEmailAndCode: ({
+    email,
+    code,
+  }: {
+    email: string;
+    code: string;
+  }) => Promise<IPasswordReset>;
+  insert: (
+    payload: Omit<IPasswordReset, "_id" | "created_at" | "updated_at">
+  ) => Promise<IPasswordReset>;
+  update: (payload: IPasswordReset) => Promise<IPasswordReset>;
+  hardDelete: ({ _id }: { _id: string }) => Promise<IPasswordReset>;
+}
