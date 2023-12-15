f9824b8a5b003f2ce1d6bac59598cb219ebad77a
huytran17 <alpha.lloyd1368@gmail.com>
2023-12-16 00:14:42 +0700

    password reset router

--- server/src/routes/user/index.ts
+++ server/src/routes/user/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { auth_router } from "./auth";
+import { password_reset_router } from "./password-reset";
 
 const user_router = Router();
 
 user_router.use("/auth", auth_router);
+user_router.use("/password-reset", password_reset_router);
 
 export { user_router };
---
+++ server/src/routes/user/password-reset.ts
@@ -0,0 +1,27 @@
+import makeExpressCallback from "@/config/middlewares/express/make-express-callback";
+import makeValidator from "@/config/middlewares/validator/make-validator";
+import {
+  verifyPasswordResetController,
+  sendPasswordResetEmailController,
+} from "@/controllers/user/password-reset";
+import {
+  reset_password_validator,
+  send_password_reset_email_validator,
+} from "@/controllers/user/password-reset/validator";
+import { Router } from "express";
+
+const password_reset_router = Router();
+
+password_reset_router.post(
+  "/send-password-reset-email",
+  makeValidator(send_password_reset_email_validator),
+  makeExpressCallback(sendPasswordResetEmailController)
+);
+
+password_reset_router.put(
+  "/reset-password",
+  makeValidator(reset_password_validator),
+  makeExpressCallback(verifyPasswordResetController)
+);
+
+export { password_reset_router };
