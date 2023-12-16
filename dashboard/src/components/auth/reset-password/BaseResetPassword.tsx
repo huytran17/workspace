f5bb0b4182c6b5906829ff081e824c2133a1dfa4
huytran17 <alpha.lloyd1368@gmail.com>
2023-12-16 13:14:29 +0700

    reset password form

---
+++ dashboard/src/components/auth/reset-password/BaseResetPassword.tsx
@@ -0,0 +1,78 @@
+import { useRootDispatch } from "@/hooks/redux";
+import { RESET_PASSWORD } from "@/store/auth/actions/actions";
+import { passwordResetRules } from "@/validation/password-reset";
+import { Button, Form, Input } from "antd";
+import { FC, useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+type ResetPasswordDetails = {
+  password: string;
+  password_confirmation: string;
+};
+
+const BaseResetPassword: FC<{}> = () => {
+  const dispatch = useRootDispatch();
+  const [searchParams] = useSearchParams();
+  const [submittable, setSubmittable] = useState(false);
+
+  const [form] = Form.useForm();
+  const values = Form.useWatch([], form);
+
+  useEffect(() => {
+    form.validateFields({ validateOnly: true }).then(
+      () => setSubmittable(true),
+      () => setSubmittable(false)
+    );
+  }, [values]);
+
+  const resetPassword = () => {
+    const payload = {
+      token: searchParams.get("token") || "",
+      password: form.getFieldValue("password"),
+      password_confirmation: form.getFieldValue("password_confirmation"),
+    };
+    dispatch(RESET_PASSWORD(payload));
+  };
+
+  return (
+    <div className="reset-password">
+      <Form
+        form={form}
+        name="basic"
+        style={{ maxWidth: 600 }}
+        autoComplete="off"
+        layout="vertical"
+        scrollToFirstError
+      >
+        <Form.Item<ResetPasswordDetails>
+          label="Password"
+          name="password"
+          rules={passwordResetRules.password}
+        >
+          <Input.Password name="password" size="large" />
+        </Form.Item>
+
+        <Form.Item<ResetPasswordDetails>
+          label="Password confirmation"
+          name="password_confirmation"
+          rules={passwordResetRules.password_confirmation}
+        >
+          <Input.Password name="password_confirmation" size="large" />
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            disabled={!submittable}
+            onClick={() => resetPassword()}
+          >
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default BaseResetPassword;
---
+++ dashboard/src/pages/auth/reset-password/index.tsx
@@ -0,0 +1,8 @@
+import BaseResetPassword from "@/components/auth/reset-password/BaseResetPassword";
+import { FC } from "react";
+
+const ResetPasswordPage: FC<{}> = () => {
+  return <BaseResetPassword />;
+};
+
+export default ResetPasswordPage;
