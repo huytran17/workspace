78ebf70613787bf476d1977b4ae7f4cb60866d1f
huytran17 <alpha.lloyd1368@gmail.com>
2023-12-24 21:47:35 +0700

    cleanup code

--- server/src/data-access/interfaces/password-reset-db.ts
+++ server/src/data-access/interfaces/password-reset-db.ts
@@ -1,5 +1,7 @@
 import IPasswordReset from "@/database/interfaces/password-reset";
 
+export type PayloadOmitProps = "_id" | "created_at" | "updated_at";
+
 export default interface IPasswordResetDb {
   findByEmailAndCode: ({
     email,
@@ -11,7 +13,7 @@ export default interface IPasswordResetDb {
   findByCode: ({ code }: { code: number }) => Promise<IPasswordReset>;
   findByEmail: ({ email }: { email: string }) => Promise<IPasswordReset>;
   insert: (
-    payload: Omit<IPasswordReset, "_id" | "created_at" | "updated_at">
+    payload: Omit<IPasswordReset, PayloadOmitProps>
   ) => Promise<IPasswordReset>;
   update: (payload: IPasswordReset) => Promise<IPasswordReset>;
   hardDelete: ({ _id }: { _id: string }) => Promise<IPasswordReset>;
--- server/src/data-access/interfaces/user-db.ts
+++ server/src/data-access/interfaces/user-db.ts
@@ -1,12 +1,16 @@
 import IUser from "@/database/interfaces/user";
 
+export type PayloadOmitProps =
+  | "_id"
+  | "created_at"
+  | "updated_at"
+  | "deleted_at";
+
 export default interface IUserDb {
   findAll: () => Promise<IUser[]>;
   findById: ({ _id }: { _id: string }) => Promise<IUser>;
   findByEmail: ({ email }: { email: string }) => Promise<IUser>;
-  insert: (
-    payload: Omit<IUser, "_id" | "created_at" | "updated_at" | "deleted_at">
-  ) => Promise<IUser>;
+  insert: (payload: Omit<IUser, PayloadOmitProps>) => Promise<IUser>;
   update: (payload: Partial<IUser>) => Promise<IUser>;
   delete: ({ _id }: { _id: string }) => Promise<IUser>;
   hardDelete: ({ _id }: { _id: string }) => Promise<IUser>;
