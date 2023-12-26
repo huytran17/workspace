19dc52bcc29a03fe3ab8b5c1be5fb409e0b23556
huytran17 <alpha.lloyd1368@gmail.com>
2023-12-26 20:17:39 +0700

    getOrdersByUserPaginated

--- server/src/use-cases/order/index.ts
+++ server/src/use-cases/order/index.ts
@@ -1,5 +1,6 @@
 import makeCreateOrder from "./create-order";
 import makeDeleteOrder from "./delete-order";
+import makeGetOrdersByUserPaginated from "./get-orders-by-user-paginated";
 import makeHardDeleteOrder from "./hard-delete-order";
 import makeUpdateOrder from "./update-order";
 
@@ -9,15 +10,22 @@ const createOrder = makeCreateOrder({ orderDb });
 const deleteOrder = makeDeleteOrder({ orderDb });
 const hardDeleteOrder = makeHardDeleteOrder({ orderDb });
 const updateOrder = makeUpdateOrder({ orderDb });
+const getOrdersByUserPaginated = makeGetOrdersByUserPaginated({ orderDb });
 
 const orderServices = Object.freeze({
   createOrder,
   deleteOrder,
   hardDeleteOrder,
   updateOrder,
+  getOrdersByUserPaginated,
 });
 
 export default orderServices;
 
-export { createOrder, deleteOrder, hardDeleteOrder, updateOrder };
-
+export {
+  createOrder,
+  deleteOrder,
+  getOrdersByUserPaginated,
+  hardDeleteOrder,
+  updateOrder,
+};
